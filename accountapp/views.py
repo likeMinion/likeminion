b35151f3df1bad498a57d7ba9fbb84b41eb5209e
pksmb <pksmb3030@gmail.com>
2021-07-08 12:35:09 +0900

    7월 8일 request.method POST GET

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,5 +12,14 @@ from django.shortcuts import render
 #     return render(request, 'base.html')
 
 # 7월 5일
+# def hello_world(request):
+#     return render(request, 'accountapp/hello_world.html')
+
+# 7월 8일
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == 'POST':
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text':'POST METHOD'})
+    else:
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text':'GET METHOD'})
