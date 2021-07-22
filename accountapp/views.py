7e29dc66091ed83a1f45b40e87eeb93d2e0fcb33
EndOfStart <tkcldqjrm1@naver.com>
2021-07-22 11:24:03 +0900

    7월 22일 -1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
@@ -25,7 +26,8 @@ from django.shortcuts import render
 #                       context={'text':'GET METHOD'})
 
 # 7월 12일
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 
 from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
@@ -52,23 +54,41 @@ from accountapp.models import HelloWorld
 #                       context={'hello_world_list':hello_world_list})
 
 from accountapp.models import HelloWorld
-def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            temp = request.POST.get('input_text')  # request 안에 POST 내의 값을 가져옴
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()  # 데이터 베이스 안에 새로운 행이 만들어짐
 
-            # Get 부분과 중복되는 코드 삭제함
-            # 실행해서 페이지에서 새로고침할 때 중복 코드 입력되는 것을 방지함
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()  # 모든 데이터를 가져옴
-            return render(request, 'accountapp/hello_world.html',
-                          context={'hello_world_list': hello_world_list})
+# def hello_world(request):
+#     if request.user.is_authenticated:
+#         if request.method == 'POST':
+#             temp = request.POST.get('input_text')  # request 안에 POST 내의 값을 가져옴
+#             new_hello_world = HelloWorld()
+#             new_hello_world.text = temp
+#             new_hello_world.save()  # 데이터 베이스 안에 새로운 행이 만들어짐
+#
+#             # Get 부분과 중복되는 코드 삭제함
+#             # 실행해서 페이지에서 새로고침할 때 중복 코드 입력되는 것을 방지함
+#             return HttpResponseRedirect(reverse('accountapp:hello_world'))
+#         else:
+#             hello_world_list = HelloWorld.objects.all()  # 모든 데이터를 가져옴
+#             return render(request, 'accountapp/hello_world.html',
+#                           context={'hello_world_list': hello_world_list})
+#     else:
+#         return HttpResponseRedirect(reverse('accountapp:login'))
+
+# 7월 22일
+@login_required(login_url=reverse_lazy('accountapp:lonin'))
+def hello_world(request):
+    if request.method == 'POST':
+        temp = request.POST.get('input_text')  # request 안에 POST 내의 값을 가져옴
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()  # 데이터 베이스 안에 새로운 행이 만들어짐
+
+        # Get 부분과 중복되는 코드 삭제함
+        # 실행해서 페이지에서 새로고침할 때 중복 코드 입력되는 것을 방지함
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = HelloWorld.objects.all()  # 모든 데이터를 가져옴
+        return render(request, 'accountapp/hello_world.html',
+                      context={'hello_world_list': hello_world_list})
 
 # 7월 13일
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
@@ -90,6 +110,8 @@ class AccountDetailView(DetailView):
 
 
 # 7월 19일
+@method_decorator(login_required, 'get') # 7월 22일
+@method_decorator(login_required, 'post') # 7월 22일
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm # UserCreationForm
@@ -98,19 +120,21 @@ class AccountUpdateView(UpdateView):
     template_name = 'accountapp/update.html'
 
     # 7월 20일
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
+    # def get(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user:
+    #         return super().get(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
+    #
+    # def post(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user:
+    #         return super().post(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
 
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-
+@method_decorator(login_required, 'get') # 7월 22일
+@method_decorator(login_required, 'post') # 7월 22일
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
@@ -118,14 +142,15 @@ class AccountDeleteView(DeleteView):
     template_name = 'accountapp/delete.html'
 
     # 7월 20일
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
+    # def get(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user:
+    #         return super().get(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
+    #
+    # def post(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user:
+    #         return super().post(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
 
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
