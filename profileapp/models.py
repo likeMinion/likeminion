a9034bc6d4d93dd4930fcddbf97278e5b4d7f828
EndOfStart <tkcldqjrm1@naver.com>
2021-07-26 11:33:18 +0900

    7월 26일 -1

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+# 7월 26월
+class Profile(models.Model):
+    # CASCADE : 종속
+    # 삭제됐을 때 종속된 모델도 같이 삭제한다는 의미
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=30, unique=True, null=True)
+    message = models.CharField(max_length=200, null=True)
