98fea51ec4c8ac005f684c84c74b0252b00fa031
EndOfStart <tkcldqjrm1@naver.com>
2021-08-03 18:11:53 +0900

    8월 3일-4

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', ArticleDetailView.as_view(), name='delete'),
 ]
